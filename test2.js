1538d694cc80d135028225f195812dc03d8bd978
arsalangithub <ahmedarsalan030@gmail.com>
2023-02-14 15:51:04 +0500

    Qouestion 3rd of typscript example

---
+++ test2.js
@@ -0,0 +1,3 @@
+var Author_Name = "Elon Musk  :";
+var Qoute = "'Some people don't like change, but you need to embrace change if the alternative is disaster. ...'";
+console.log(Author_Name + Qoute);
---
+++ test2.ts
@@ -0,0 +1,3 @@
+let Author_Name = "Elon Musk  :"
+let Qoute = "'Some people don't like change, but you need to embrace change if the alternative is disaster. ...'"
+console.log(Author_Name + Qoute);
